4c39f0e1e9a76dd691191ec7b91face3ea264414
unknown <DisaChristanto>
2015-10-01 17:27:31 +0700

    test

---
+++ MI2D_1431140088_DisaChristanto/WindowsApplication4/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+
+    Private Sub layarButton_Click(sender As Object, e As EventArgs)
+        
+    End Sub
+
+    Private Sub ubahButton_Click(sender As Object, e As EventArgs) Handles ubahButton.Click
+        If Me.BackColor() = Color.Chocolate Then
+            Me.BackColor() = Color.White
+        Else
+            Me.BackColor() = Color.Chocolate
+        End If
+    End Sub
+End Class
