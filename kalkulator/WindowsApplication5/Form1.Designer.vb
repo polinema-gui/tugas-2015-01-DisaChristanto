f2c5f799c3a106f3a54129b3475eab4c7ab64d7f
unknown <DisaChristanto>
2015-10-11 12:41:37 +0700

    TugasKalkulator

---
+++ kalkulator/WindowsApplication5/Form1.Designer.vb
@@ -0,0 +1,235 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Bt0 = New System.Windows.Forms.Button()
+        Me.Bt1 = New System.Windows.Forms.Button()
+        Me.Bt4 = New System.Windows.Forms.Button()
+        Me.Bt7 = New System.Windows.Forms.Button()
+        Me.btsmdn = New System.Windows.Forms.Button()
+        Me.Bt2 = New System.Windows.Forms.Button()
+        Me.bt3 = New System.Windows.Forms.Button()
+        Me.Bt5 = New System.Windows.Forms.Button()
+        Me.Bt8 = New System.Windows.Forms.Button()
+        Me.bt6 = New System.Windows.Forms.Button()
+        Me.Bt9 = New System.Windows.Forms.Button()
+        Me.btpls = New System.Windows.Forms.Button()
+        Me.btkrng = New System.Windows.Forms.Button()
+        Me.btclr = New System.Windows.Forms.Button()
+        Me.Background = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(47, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.TextBox1.Size = New System.Drawing.Size(319, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "0"
+        '
+        'Bt0
+        '
+        Me.Bt0.Location = New System.Drawing.Point(46, 202)
+        Me.Bt0.Name = "Bt0"
+        Me.Bt0.Size = New System.Drawing.Size(75, 45)
+        Me.Bt0.TabIndex = 1
+        Me.Bt0.Text = "0"
+        Me.Bt0.UseVisualStyleBackColor = True
+        '
+        'Bt1
+        '
+        Me.Bt1.Location = New System.Drawing.Point(46, 151)
+        Me.Bt1.Name = "Bt1"
+        Me.Bt1.Size = New System.Drawing.Size(75, 45)
+        Me.Bt1.TabIndex = 2
+        Me.Bt1.Text = "1"
+        Me.Bt1.UseVisualStyleBackColor = True
+        '
+        'Bt4
+        '
+        Me.Bt4.Location = New System.Drawing.Point(46, 100)
+        Me.Bt4.Name = "Bt4"
+        Me.Bt4.Size = New System.Drawing.Size(75, 45)
+        Me.Bt4.TabIndex = 3
+        Me.Bt4.Text = "4"
+        Me.Bt4.UseVisualStyleBackColor = True
+        '
+        'Bt7
+        '
+        Me.Bt7.Location = New System.Drawing.Point(45, 49)
+        Me.Bt7.Name = "Bt7"
+        Me.Bt7.Size = New System.Drawing.Size(75, 45)
+        Me.Bt7.TabIndex = 4
+        Me.Bt7.Text = "7"
+        Me.Bt7.UseVisualStyleBackColor = True
+        '
+        'btsmdn
+        '
+        Me.btsmdn.Location = New System.Drawing.Point(127, 202)
+        Me.btsmdn.Name = "btsmdn"
+        Me.btsmdn.Size = New System.Drawing.Size(157, 45)
+        Me.btsmdn.TabIndex = 5
+        Me.btsmdn.Text = "="
+        Me.btsmdn.UseVisualStyleBackColor = True
+        '
+        'Bt2
+        '
+        Me.Bt2.Location = New System.Drawing.Point(127, 151)
+        Me.Bt2.Name = "Bt2"
+        Me.Bt2.Size = New System.Drawing.Size(75, 45)
+        Me.Bt2.TabIndex = 6
+        Me.Bt2.Text = "2"
+        Me.Bt2.UseVisualStyleBackColor = True
+        '
+        'bt3
+        '
+        Me.bt3.Location = New System.Drawing.Point(209, 151)
+        Me.bt3.Name = "bt3"
+        Me.bt3.Size = New System.Drawing.Size(75, 45)
+        Me.bt3.TabIndex = 7
+        Me.bt3.Text = "3"
+        Me.bt3.UseVisualStyleBackColor = True
+        '
+        'Bt5
+        '
+        Me.Bt5.Location = New System.Drawing.Point(127, 100)
+        Me.Bt5.Name = "Bt5"
+        Me.Bt5.Size = New System.Drawing.Size(75, 45)
+        Me.Bt5.TabIndex = 8
+        Me.Bt5.Text = "5"
+        Me.Bt5.UseVisualStyleBackColor = True
+        '
+        'Bt8
+        '
+        Me.Bt8.Location = New System.Drawing.Point(126, 49)
+        Me.Bt8.Name = "Bt8"
+        Me.Bt8.Size = New System.Drawing.Size(75, 45)
+        Me.Bt8.TabIndex = 9
+        Me.Bt8.Text = "8"
+        Me.Bt8.UseVisualStyleBackColor = True
+        '
+        'bt6
+        '
+        Me.bt6.Location = New System.Drawing.Point(209, 100)
+        Me.bt6.Name = "bt6"
+        Me.bt6.Size = New System.Drawing.Size(75, 45)
+        Me.bt6.TabIndex = 10
+        Me.bt6.Text = "6"
+        Me.bt6.UseVisualStyleBackColor = True
+        '
+        'Bt9
+        '
+        Me.Bt9.Location = New System.Drawing.Point(209, 49)
+        Me.Bt9.Name = "Bt9"
+        Me.Bt9.Size = New System.Drawing.Size(75, 45)
+        Me.Bt9.TabIndex = 11
+        Me.Bt9.Text = "9"
+        Me.Bt9.UseVisualStyleBackColor = True
+        '
+        'btpls
+        '
+        Me.btpls.Location = New System.Drawing.Point(289, 202)
+        Me.btpls.Name = "btpls"
+        Me.btpls.Size = New System.Drawing.Size(77, 45)
+        Me.btpls.TabIndex = 12
+        Me.btpls.Text = "+"
+        Me.btpls.UseVisualStyleBackColor = True
+        '
+        'btkrng
+        '
+        Me.btkrng.Location = New System.Drawing.Point(291, 151)
+        Me.btkrng.Name = "btkrng"
+        Me.btkrng.Size = New System.Drawing.Size(75, 45)
+        Me.btkrng.TabIndex = 13
+        Me.btkrng.Text = "-"
+        Me.btkrng.UseVisualStyleBackColor = True
+        '
+        'btclr
+        '
+        Me.btclr.Location = New System.Drawing.Point(291, 49)
+        Me.btclr.Name = "btclr"
+        Me.btclr.Size = New System.Drawing.Size(75, 96)
+        Me.btclr.TabIndex = 14
+        Me.btclr.Text = "clear"
+        Me.btclr.UseVisualStyleBackColor = True
+        '
+        'Background
+        '
+        Me.Background.Location = New System.Drawing.Point(47, 265)
+        Me.Background.Name = "Background"
+        Me.Background.Size = New System.Drawing.Size(318, 26)
+        Me.Background.TabIndex = 15
+        Me.Background.Text = "Change Background"
+        Me.Background.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(411, 304)
+        Me.Controls.Add(Me.Background)
+        Me.Controls.Add(Me.btclr)
+        Me.Controls.Add(Me.btkrng)
+        Me.Controls.Add(Me.btpls)
+        Me.Controls.Add(Me.Bt9)
+        Me.Controls.Add(Me.bt6)
+        Me.Controls.Add(Me.Bt8)
+        Me.Controls.Add(Me.Bt5)
+        Me.Controls.Add(Me.bt3)
+        Me.Controls.Add(Me.Bt2)
+        Me.Controls.Add(Me.btsmdn)
+        Me.Controls.Add(Me.Bt7)
+        Me.Controls.Add(Me.Bt4)
+        Me.Controls.Add(Me.Bt1)
+        Me.Controls.Add(Me.Bt0)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Bt0 As System.Windows.Forms.Button
+    Friend WithEvents Bt1 As System.Windows.Forms.Button
+    Friend WithEvents Bt4 As System.Windows.Forms.Button
+    Friend WithEvents Bt7 As System.Windows.Forms.Button
+    Friend WithEvents btsmdn As System.Windows.Forms.Button
+    Friend WithEvents Bt2 As System.Windows.Forms.Button
+    Friend WithEvents bt3 As System.Windows.Forms.Button
+    Friend WithEvents Bt5 As System.Windows.Forms.Button
+    Friend WithEvents Bt8 As System.Windows.Forms.Button
+    Friend WithEvents bt6 As System.Windows.Forms.Button
+    Friend WithEvents Bt9 As System.Windows.Forms.Button
+    Friend WithEvents btpls As System.Windows.Forms.Button
+    Friend WithEvents btkrng As System.Windows.Forms.Button
+    Friend WithEvents btclr As System.Windows.Forms.Button
+    Friend WithEvents Background As System.Windows.Forms.Button
+
+End Class
---
+++ kalkulator/WindowsApplication5/Form1.vb
@@ -0,0 +1,135 @@
+﻿Public Class Form1
+    Dim f As Integer
+    Dim hasil As Byte
+
+    
+    Private Sub Bt0_Click(sender As Object, e As EventArgs) Handles Bt0.Click
+        If (cekIsi()) Then
+            TextBox1.Text = "0"
+        Else
+            TextBox1.Text &= "0"
+        End If
+    End Sub
+    Private Sub Bt1_Click(sender As Object, e As EventArgs) Handles Bt1.Click
+        If (cekIsi()) Then
+            TextBox1.Text = "1"
+        Else
+            TextBox1.Text &= "1"
+        End If
+    End Sub
+    Private Sub Bt2_Click(sender As Object, e As EventArgs) Handles Bt2.Click
+        If (cekIsi()) Then
+            TextBox1.Text = "2"
+        Else
+            TextBox1.Text &= "2"
+        End If
+    End Sub
+    Private Sub Bt3_Click(sender As Object, e As EventArgs) Handles bt3.Click
+        If (cekIsi()) Then
+            TextBox1.Text = "3"
+        Else
+            TextBox1.Text &= "3"
+        End If
+    End Sub
+    Private Sub Bt4_Click(sender As Object, e As EventArgs) Handles Bt4.Click
+        If (cekIsi()) Then
+            TextBox1.Text = "4"
+        Else
+            TextBox1.Text &= "4"
+        End If
+    End Sub
+   
+    Private Sub Bt6_Click(sender As Object, e As EventArgs) Handles bt6.Click
+        If (cekIsi()) Then
+            TextBox1.Text = "6"
+        Else
+            TextBox1.Text &= "6"
+        End If
+    End Sub
+    Private Sub Bt7_Click(sender As Object, e As EventArgs) Handles Bt7.Click
+        If (cekIsi()) Then
+            TextBox1.Text = "7"
+        Else
+            TextBox1.Text &= "7"
+        End If
+    End Sub
+    Private Sub Bt8_Click(sender As Object, e As EventArgs) Handles Bt8.Click
+        If (cekIsi()) Then
+            TextBox1.Text = "8"
+        Else
+            TextBox1.Text &= "8"
+        End If
+    End Sub
+    Private Sub Bt5_Click(sender As Object, e As EventArgs) Handles Bt9.Click
+        If (cekIsi()) Then
+            TextBox1.Text = "9"
+        Else
+            TextBox1.Text &= "9"
+        End If
+    End Sub
+    Private Function cekIsi()
+        If (TextBox1.Text.Equals("0") Or Me.hasil> 0) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+    Private Sub ButtonHapus_Click(sender As Object, e As EventArgs) Handles btclr.Click
+        TextBox1.Text = "0"
+        Me.f = 0
+        Me.hasil = 0
+    End Sub
+
+    Private Sub Btkrng_Click(sender As Object, e As EventArgs) Handles btkrng.Click
+        If (Me.f = 0 Or Me.hasil = 2) Then
+            Me.f += Convert.ToInt32(TextBox1.Text)
+        Else
+            Me.f -= Convert.ToInt32(TextBox1.Text)
+        End If
+        TextBox1.Text = Me.f.ToString
+        Me.hasil = 1
+    End Sub
+   
+
+    Private Sub Bt5_Click_1(sender As Object, e As EventArgs) Handles Bt5.Click
+        If (cekIsi()) Then
+            TextBox1.Text = "5"
+        Else
+            TextBox1.Text &= "5"
+        End If
+    End Sub
+
+    Private Sub btsmdn_Click(sender As Object, e As EventArgs) Handles btsmdn.Click
+        Select Case Me.hasil
+
+            Case 1
+                TextBox1.Text = Convert.ToString(Me.f - Convert.ToInt32(TextBox1.Text))
+            Case 2
+                TextBox1.Text = Convert.ToString(Me.f + Convert.ToInt32(TextBox1.Text))
+        End Select
+        Me.f = 0
+    End Sub
+
+    Private Sub btpls_Click(sender As Object, e As EventArgs) Handles btpls.Click
+        If (Me.hasil = 1) Then
+            Me.f -= Convert.ToInt32(TextBox1.Text)
+        Else
+            Me.f += Convert.ToInt32(TextBox1.Text)
+        End If
+        TextBox1.Text = Me.f.ToString
+        Me.hasil = 2
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Background_Click(sender As Object, e As EventArgs) Handles Background.Click
+        If Me.BackColor() = Color.White Then
+            Me.BackColor() = Color.Black
+        Else
+            Me.BackColor() = Color.White
+        End If
+    End Sub
+End Class
+
